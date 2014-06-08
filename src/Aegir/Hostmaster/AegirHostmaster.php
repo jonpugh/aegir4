a07cb0c451861cb76d1f0aafc316f40fe6a50b5e
Jon Pugh <jon@thinkdrop.net>
2014-06-08 11:35:32 -0400

    Adding bundle classes

---
+++ src/Aegir/Hostmaster/AegirHostmaster.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aegir\Hostmaster;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AegirHostmaster extends Bundle
+{
+}
---
+++ src/Aegir/Provision/AegirProvision.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aegir\Provision;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AegirProvision extends Bundle
+{
+}
