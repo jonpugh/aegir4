7ab989f6b9e75f4ce6cebfabfba587056950ec9b
Jon Pugh <jon@thinkdrop.net>
2014-08-11 10:54:45 -0400

    Attempting to detect IP addresses automatically.

--- src/Aegir/Provision/Model/Server.php
+++ src/Aegir/Provision/Model/Server.php
@@ -30,11 +30,26 @@ class Server {
   public $services;
 
   /**
-   * Initiate the server
+   * Initiate the server.
+   *
+   * The first time this is initiated
    */
-  public function __construct($hostname, $ip_addresses, $services) {
+  public function __construct($hostname, $ip_addresses = '', $services = array()) {
     $this->hostname = $hostname;
-    $this->ip_addresses[] = $ip_addresses;
+
+
+    if (!empty($hostname) && $ip_addresses == '') {
+      $this->ip_addresses[] = gethostbyname($hostname);
+    }
+    else {
+      $this->ip_addresses[] = $ip_addresses;
+    }
+
     $this->services = $services;
+
+    if (!empty($this->hostname)){
+      print_r($this);
+      die;
+    }
   }
 }
